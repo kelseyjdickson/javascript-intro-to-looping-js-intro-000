bc508eb199d04210f68849cb2c77e8296c8884aa
Kelsey Dickson <kelseymedved@gmail.com>
2020-04-23 18:23:54 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,33 +1,18 @@
 function forLoop(array){
   for( let i = 0 ; i < 25; i++){
     if(i === 1){
-    array.push(`I am 1 strange loop.`);
+    array.push(`I am 1 strange loop.`)
   } else {
     array.push(`I am ${i} strange loops.`)
   }
 }
   return array;
 }
-
-function whileLoop(num){
-  while(num > 0){
-    console.log(num--);
-  }
-  return 'done';
-}
-
-
 let i = 0;
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
-function doWhileLoop(num){
-  do {
-  console.log(`I run once regardless`);
-  } while(incrementVariable() < num);
-num--;
+function whileLoop(number){
+  while( number < i) {
+    console.log(number)
+    i--;
   }
-
+return 'done'
 }
